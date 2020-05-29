a432242e8dc9883f45434a3bc7fa28bde3543b4b
Ubuntu <ubuntu@ip-172-31-46-195.us-west-2.compute.internal>
2020-05-29 06:49:27 +0000

    amend

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ import torch
 
 if __name__ == '__main__':
     torch.set_num_threads(4)
+    torch.set_num_interop_threads(4)
     parser = argparse.ArgumentParser(description='Thompson Sampling')
     parser.add_argument('--encoding', default='multi', metavar='sanity|onehot|multi')
 
