256bf8378d69b4c381ea2fc9a4d496feb04687ea
Ubuntu <ubuntu@ip-172-31-13-38.us-west-1.compute.internal>
2020-11-16 04:13:46 +0000

    rm

--- cpu_run.sh
+++ cpu_run.sh
@@ -1,17 +1,5 @@
 for ((n=0;n<16;n++))
 do
-	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset mnist
-	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset mushroom
-	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset shuttle
-	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset covertype
-	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset MagicTelescope
-	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset adult
-	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset mnist
-	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset mushroom
-	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset shuttle
-	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset covertype
-	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset MagicTelescope
-	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset adult
 	python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset mushroom 
 	python3 train.py --encoding multi --learner linear --style ts --nu 0.1 --lamdba 1 --inv full --dataset mushroom
 	python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset shuttle
