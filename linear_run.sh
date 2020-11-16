0a939af57aae54c232826c76639b51468e403ff3
Ubuntu <ubuntu@ip-172-31-13-38.us-west-1.compute.internal>
2020-11-16 19:39:23 +0000

    mnist linear

--- linear_run.sh
+++ linear_run.sh
@@ -1,12 +1,4 @@
-python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset mushroom 
-python3 train.py --encoding multi --learner linear --style ts --nu 0.1 --lamdba 1 --inv full --dataset mushroom
-python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset shuttle
-python3 train.py --encoding multi --learner linear --style ts --nu 0.1 --lamdba 1 --inv full --dataset shuttle
-python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset adult
-python3 train.py --encoding multi --learner linear --style ts --nu 0.1 --lamdba 1 --inv full --dataset adult
-python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset MagicTelescope
-python3 train.py --encoding multi --learner linear --style ts --nu 1 --lamdba 1 --inv full --dataset MagicTelescope
-python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset covertype
-python3 train.py --encoding multi --learner linear --style ts --nu 1 --lamdba 1 --inv full --dataset covertype
-python3 train.py --encoding multi --learner linear --style ucb --nu 10 --lamdba 1 --inv full --dataset letter
-python3 train.py --encoding multi --learner linear --style ts --nu 0.1 --lamdba 1 --inv full --dataset letter
+for((n=0;n<32;n++))
+do
+	python3 train.py --encoding multi --learner linear --style ucb --nu 1 --lamdba 1 --inv full --dataset mnist
+done
