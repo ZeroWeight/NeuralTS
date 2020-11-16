5388827a77f07eb3bad945a67c61f417baa3666d
Ubuntu <ubuntu@ip-172-31-34-164.us-west-2.compute.internal>
2020-11-16 04:24:12 +0000

    add kernel

--- kernel.sh
+++ kernel.sh
@@ -1,12 +1,15 @@
-python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset mnist
-python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset mushroom
-python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset shuttle
-python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset covertype
-python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset MagicTelescope
-python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset adult
-python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset mnist
-python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset mushroom
-python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset shuttle
-python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset covertype
-python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset MagicTelescope
-python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset adult
+for((n=0;n<32;n++))
+do
+	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset mnist
+	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset mushroom
+	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset shuttle
+	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset covertype
+	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset MagicTelescope
+	python3 train.py --encoding multi --learner kernel --style ucb --nu 0.1 --lamdba 1 --inv full --dataset adult
+	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset mnist
+	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset mushroom
+	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset shuttle
+	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset covertype
+	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset MagicTelescope
+	python3 train.py --encoding multi --learner kernel --style ts --nu 0.01 --lamdba 1 --inv full --dataset adult
+done
